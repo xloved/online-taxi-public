d2685c9246f683db1142a30fc2ce932f7b63f10e
xloved <346570846@qq.com>
2022-09-19 21:16:22 +0800

    判断用户是否存在，不存在插入用户信息

--- online-taxi-public/service-passenger-user/src/main/java/com/hgx/servicepassengeruser/mapper/PassengerUserMapper.java
+++ online-taxi-public/service-passenger-user/src/main/java/com/hgx/servicepassengeruser/mapper/PassengerUserMapper.java
@@ -3,7 +3,7 @@ package com.hgx.servicepassengeruser.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hgx.servicepassengeruser.dto.PassengerUser;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
+
 
 
 @Mapper
--- online-taxi-public/service-passenger-user/src/main/java/com/hgx/servicepassengeruser/service/UserService.java
+++ online-taxi-public/service-passenger-user/src/main/java/com/hgx/servicepassengeruser/service/UserService.java
@@ -3,9 +3,11 @@ package com.hgx.servicepassengeruser.service;
 import com.hgx.internalcomm.dto.ResponseResult;
 import com.hgx.servicepassengeruser.dto.PassengerUser;
 import com.hgx.servicepassengeruser.mapper.PassengerUserMapper;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,8 +18,11 @@ public class UserService {
     @Resource
     PassengerUserMapper passengerUserMapper;
 
+    @Resource
+    PassengerUserMapper passengerUserMapper;
+
     //用户登录与注册
-    public ResponseResult loginOrRegister(String passengerPgone){
+    public ResponseResult loginOrRegister(String passengerPhone){
 
         System.out.println("user serice被调用，手机号"+passengerPgone);
         //根据手机号查询用户信息
@@ -28,7 +33,19 @@ public class UserService {
 
 
         //判断用户是否已存在
-        //如果不存在插入用户信息
+        if(passengerUsers.size() == 0){
+            //如果不存在插入用户信息
+            PassengerUser passengerUser = new PassengerUser( );
+            passengerUser.setPassengerName("张三");
+            passengerUser.setPassengerPhone(passengerPhone);
+            LocalDateTime nowdata = LocalDateTime.now();
+            passengerUser.setGmtCreate(nowdata);
+            passengerUser.setGmtModified(nowdata);
+            passengerUser.setPassengerGender((byte)0);
+            passengerUser.setState((byte)0);
+            passengerUserMapper.insert(passengerUser);
+        }
+
 
         return ResponseResult.success();
     }
