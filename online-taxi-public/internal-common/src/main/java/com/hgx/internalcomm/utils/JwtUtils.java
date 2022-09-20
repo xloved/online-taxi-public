89db5a6ae9b4cda5c9068254d897d7ea8bb4be8b
xloved <346570846@qq.com>
2022-09-20 21:56:44 +0800

    引入jwt工具，进行token的生成

---
+++ online-taxi-public/internal-common/src/main/java/com/hgx/internalcomm/utils/JwtUtils.java
@@ -0,0 +1,49 @@
+package com.hgx.internalcomm.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtils {
+
+    //设置盐
+
+    private  static final String SING = "CFsnm#@.*!";
+
+    //生成token
+    public static String generatorToken(Map<String,String> map){
+        //获取当前时间
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE,30);//设置过期时间
+        Date date = calendar.getTime();//具体的到期时间点时间
+        JWTCreator.Builder builder = JWT.create();//生成builder
+        //使用lamde表达式迭代map
+        map.forEach((k,v)-> {
+                builder.withClaim(k,v);
+        });
+
+        //整合过期时间
+        builder.withExpiresAt(date);
+
+        //生成token
+        String singer = builder.sign(Algorithm.HMAC256(SING));
+
+        return singer;
+
+    }
+
+    //解析token
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        map.put("name","zhangsan");
+        map.put("age","18");
+         String s = generatorToken(map);
+        System.out.println("生成的token："+s);
+    }
+
+}
