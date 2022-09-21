74385d4194cb0e1678fc5f3b7aed97cd88fceb70
xloved <346570846@qq.com>
2022-09-21 21:55:58 +0800

    测试token的解析

--- online-taxi-public/internal-common/src/main/java/com/hgx/internalcomm/utils/JwtUtils.java
+++ online-taxi-public/internal-common/src/main/java/com/hgx/internalcomm/utils/JwtUtils.java
@@ -3,6 +3,8 @@ package com.hgx.internalcomm.utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -15,8 +17,15 @@ public class JwtUtils {
 
     private  static final String SING = "CFsnm#@.*!";
 
+    //定义传入的key
+    private static final String JWT_KEY = "";
+
     //生成token
-    public static String generatorToken(Map<String,String> map){
+    public static String generatorToken(String passengerPhone){
+
+        Map<String,String> map  = new HashMap<>();
+        map.put(JWT_KEY,passengerPhone);
+        System.out.println(passengerPhone);
         //获取当前时间
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE,30);//设置过期时间
@@ -37,13 +46,25 @@ public class JwtUtils {
 
     }
 
+
     //解析token
+    public static String  parseToken(String token){
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SING)).build().verify(token);
+        System.out.println(verify);
+        Claim claim = verify.getClaim(JWT_KEY);
+        System.out.println(claim);
+        return claim.toString();
+    }
+
+    //测试
     public static void main(String[] args) {
-        Map<String,String> map = new HashMap<>();
+        /*Map<String,String> map = new HashMap<>();
         map.put("name","zhangsan");
-        map.put("age","18");
-         String s = generatorToken(map);
+        map.put("age","18");*/
+         String s = generatorToken("17890653409");
         System.out.println("生成的token："+s);
+
+        System.out.println("解析token："+parseToken(s));
     }
 
 }
